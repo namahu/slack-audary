845b6c77518fd0e448dfbd7c96fbf36429765a08
namahu <neti0326@gmail.com>
2021-11-06 23:45:51 +0900

    feat: Implement the process of getting the report from toggl.

--- src/Audary.ts
+++ src/Audary.ts
@@ -1,7 +1,11 @@
+import { createTogglInstance, ITogglService, RequestParams, SummaryReport } from "./lib/TogglService";
+
 export type Properties = {
     SLACK_BOT_TOKEN: string;
+    SLACK_MESSAGE_TEMPLATE: string;
     TOGGL_API_TOKEN: string;
     TOGGL_USER_AGENT: string;
+    TOGGL_WORKSPACE_ID: string;
 };
 
 export type SlackEventParameters = {
@@ -29,7 +33,62 @@ const doPost = (event: GoogleAppsScript.Events.DoPost) => {
     const properties = scriptProperty.getProperties() as unknown as Properties;
 
     scriptProperty.setProperties({
-        "event": JSON.stringify(event),
-        "parameter": JSON.stringify(parameter)
+        "event": JSON.stringify(event)
     });
+
+    const toggl: ITogglService = createTogglInstance(properties.TOGGL_API_TOKEN);
+    const today = new Date().toISOString().substr(0, 10);
+
+    const params: RequestParams = {
+        user_agent: properties.TOGGL_USER_AGENT,
+        workspace_id: Number(properties.TOGGL_WORKSPACE_ID),
+        since: today
+    };
+
+    const reportResponse = toggl.getSummaryReport(params);
+    if (!reportResponse.ok) {
+        return ask(reportResponse);
+    }
+
+    const message = createSlackMessage(
+        reportResponse.report.data as SummaryReport[],
+        today,
+        properties
+    );
+
+
+
 };
+
+const ask = (message: object) => {
+    return ContentService
+        .createTextOutput(JSON.stringify(message))
+        .setMimeType(ContentService.MimeType.JSON);
+};
+
+const convertMillisecondIntoMinutes = (milliseconds: number): number => {
+    return milliseconds / 1000 / 60;
+}
+
+const createSlackMessage = (
+    summaryReport: SummaryReport[],
+    dateString: string,
+    properties: Properties
+): string => {
+    const timeEntryList = summaryReport.flatMap(report => {
+        return report.items.map(item => {
+            return "・" + item.title.time_entry
+                + ": "
+                + Math.round(convertMillisecondIntoMinutes(item.time) * 100) / 100
+                + " 分";
+        });
+    }).join("\n");
+
+    const template = properties.SLACK_MESSAGE_TEMPLATE;
+
+    return template
+        .replace(/{{ today }}/, dateString)
+        .replace(/{{ timeEntryList }}/, timeEntryList);
+};
+
+export { createSlackMessage };
